39e7acfe9c0079ba72e88f51ce58bc7372bb2cf5
anudeepvrm <anudeepvrm@gmail.com>
2015-11-03 13:20:53 +0530

    basic info

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
 __author__ = 'avarm1'
+from distutils.core import setup
+setup(
+name = 'Hangman',
+version = '1.0.0',
+py_modules = ['Hangman'],
+author = 'anudeepvrm',
+author_email = 'anudeepvrm@gmail.com',
+description = 'A simple Hangman Game',
+)
